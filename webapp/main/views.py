964104d48a9291b00825f6a433dd0cfdd7b01473
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2015-09-21 10:52:24 -0700

    Updated .name for synonyms.

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -184,7 +184,7 @@ def view_tf(request, tfname):
             tmp = GeneSynonyms.objects.filter(gene__name=t1.gene.name).filter(synonym_type='hgnc')
             symbol = 'NA'
             if len(tmp)>0:
-                symbol = tmp[0]['name']
+                symbol = tmp[0].name
             tfbs[t1['gene__name']] = { 'symbol':symbol, 'entrez':t1.gene.name, 'num_sites':1, 'chromosome':t1.gene.chromosome, 'strand':t1.gene.orientation, 'start':t1.gene.start_promoter, 'stop':t1.gene.stop_promoter, 'tss':t1.gene.tss }
         else:
             tfbs[t1['gene__name']]['num_sites'] += 1
