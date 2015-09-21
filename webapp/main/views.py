b747062de637caf4d80a88dfb9a294958b5699d9
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2015-09-21 15:41:39 -0700

    Fixing issue with manyrelatedmanager.

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -206,7 +206,7 @@ def view_gene(request, genename):
 
     if genes.count() > 0:
         gene = genes[0]
-    motifs = [motif.name for motif in gene.motifs]
+    motifs = [motif.name for motif in gene.motifs.all()]
     return render_to_response('gene_results.html', locals())
 
 
